799e3b97d05e67fdb1163183f766533e5112aa83
OyinloluB <oyinloluwab@gmail.com>
2021-11-15 00:15:51 +0100

    completed fetch to do list

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 use rocket::*;
+use rocket_contrib::json::Json;
 use rusqlite::Connection;
+use serde::Serialize;
 
+#[derive(Serialize)]
 struct ToDoList {
     items: Vec<ToDoItem>,
 }
 
+#[derive(Serialize)]
 struct ToDoItem {
     id: i64,
     item: String,
 }
 
+#[derive(Serialize)]
 struct StatusMessage {
     message: String,
 }
@@ -19,6 +24,41 @@ struct StatusMessage {
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
+}
+
+#[get("/todo")]
+fn fetch_all_todos() -> Result<Json<ToDoList>, String> {
+    let db_connnection = match Connection::open("data.sqlite") {
+        Ok(connection) => connection,
+        Err(_) => {
+            return Err(String::from("Failed to connect to database"));
+        }
+    };
+
+    let mut statement = db_connnection
+        .prepare("select id, item from todo_list;")
+        .unwrap();
+
+    let results = statement.query_map(rusqlite::NO_PARAMS, |row| {
+        Ok(ToDoItem {
+            id: row.get(0)?,
+            item: row.get(1)?,
+        })
+    });
+
+    match results {
+        Ok(rows) => {
+            let collection: rusqlite::Result<Vec<_>> = rows.collect();
+
+            match collection {
+                Ok(items) => Ok(Json(ToDoList { items })),
+                Err(_) => Err("Could not collect items".into()),
+            }
+        }
+        Err(_) => Err("Failed to fetch todo items".into()),
+    }
+
+    Err("Unknown Error".into())
 }
 
 fn main() {
